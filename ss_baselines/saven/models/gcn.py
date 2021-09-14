51ec2171f03aaa6d08933410a596fc749f12ee50
nariaki <n.kitamura1987@gmail.com>
2021-09-14 09:56:47 -0400

    gcn

--- ss_baselines/saven/models/gcn.py
+++ ss_baselines/saven/models/gcn.py
@@ -100,3 +100,80 @@ class GCN(nn.Module):
         # print("self.final_mapping(x): ", x.shape)
 
         return x
+
+import dgl
+from dgl.nn import GraphConv
+import scipy.sparse as spp
+
+class DGL_GCN(GCN):
+    def __init__(self, in_feats, o_feats):
+        super(DGL_GCN, self).__init__()
+
+        # get and normalize adjacency matrix.
+        adjmat_path = r"data/glove_data/adjmat.bin"
+
+        with open(adjmat_path, 'rb') as f:
+            adj_mat = pickle.load(f)
+        adj_mat = adj_mat + np.eye(adj_mat.shape[0])
+        self.adj_mat = spp.coo_matrix(adj_mat)
+        self.g = dgl.DGLGraph(adj_mat)
+
+        embeddings_path = r"data/glove_data/glove_embeddings_300d.bin"
+
+        bin_file = open(embeddings_path, "rb")
+        objects_vector = pickle.load(bin_file)
+        regions_vector = pickle.load(bin_file)
+        bin_file.close()
+
+        objects = list(sorted(objects_vector.keys()))
+        regions = list(sorted(regions_vector.keys()))
+
+        self.n = len(objects) + len(regions)
+
+        # print("objects: ", len(objects), objects)
+        # print("regions: ", len(regions), regions)
+        # print("words: ", len(words), words)
+
+        all_glove = torch.zeros(self.n, 300)
+        i = 0
+        for obj in objects:
+            all_glove[i, :] = torch.Tensor(objects_vector[obj])
+            i += 1
+        for reg in regions:
+            all_glove[i, :] = torch.Tensor(regions_vector[reg])
+            i += 1
+        # print("all_glove: ", all_glove.shape)
+        # print("all_glove ", all_glove)
+
+        self.all_glove = nn.Parameter(all_glove)
+        self.all_glove.requires_grad = False
+
+        self.get_word_embed = nn.Linear(300, self.n)
+
+        #TODO
+        self.conv1 = GraphConv(in_feats, in_feats)
+        self.conv2 = GraphConv(in_feats, in_feats)
+        self.conv3 = GraphConv(in_feats, o_feats)
+
+        self.final_mapping = nn.Linear(self.n, 256-2)
+
+    def forward(self, class_embed):
+
+        class_embed = class_embed.reshape(1, -1)
+        # print("class_embed: ", class_embed.shape)
+        word_embed = self.get_word_embed(self.all_glove.detach())
+        # print("word_embed: ", word_embed.shape)
+        x = torch.cat((class_embed.repeat(self.n, 1), word_embed), dim=1)
+        # print("torch.cat((class_embed.repeat(self.n, 1), word_embed), dim=1): ", x.shape)
+
+        h = self.conv1(self.g, x)
+        h = F.relu(h)
+        h = self.conv2(self.g, h)
+        h = F.relu(h)
+        h = self.conv3(self.g, h)
+        o = F.relu(h)
+        o = o.view(1, self.g.num_nodes())
+        x = self.final_mapping(x)
+        # print("self.final_mapping(x): ", x.shape)
+
+        return x
